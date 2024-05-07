a4c4e5eab1f7aca1432b88aa1bf43d610926ca62
Fady.GamilH <Fady.Gamil@halan.com>
2024-05-08 02:35:08 +0300

    add task and task event types

--- orchestrator/task/task.go
+++ orchestrator/task/task.go
@@ -1,4 +1,30 @@
 package task
 
+import (
+	"time"
+
+	"github.com/docker/go-connections/nat"
+	"github.com/google/uuid"
+)
+
 type Task struct {
+	Id            uuid.UUID // 128 bits
+	Name          string
+	State         TaskState // at the end its a string
+	DockerImage   string    // each task will be a program running on a docker container and this container is from a specific image
+	RestartPolicy string
+	ExposedPorts  nat.PortSet // map of port to struct, where the port is a string
+	PortsBinding  map[string]string
+	Disk          int
+	Memory        int
+	StartTime     time.Time
+	EndTime       time.Time
+}
+
+// we want an event to reprsent the transition of a task from state-A to state-B (specially this is used when user need to stop or start the task)
+type TaskEvent struct {
+	Task        Task
+	Id          uuid.UUID
+	State       TaskState
+	RequestedAt time.Time
 }
